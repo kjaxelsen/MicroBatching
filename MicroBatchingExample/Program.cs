b549d3aaff66c4388f288b4fc590886f05164394
Kevin Axelsen <kevin@inspectrealestate.com.au>
2024-04-15 19:30:02 +1000

    comments

--- MicroBatchingExample/Program.cs
+++ MicroBatchingExample/Program.cs
@@ -2,29 +2,34 @@
 using MicroBatching.Models;
 using MicroBatchingExample;
 
-MicroBatchingService service = new MicroBatchingService(new BatchProcessor(), new Logger(), new MicroBatchingServiceOptions()
-{
-    MaxQueueSize = 10
-});
+// create the service and add in the batchprocessor, logger and custom service options
+MicroBatchingService service = new MicroBatchingService(new BatchProcessor(), new Logger(), new MicroBatchingServiceOptions(1000, 3, null));
+
+// start processing jobs
 service.StartProcessing();
-Console.WriteLine("Waiting 1 second to start");
-Thread.Sleep(1000);
+
+// add jobs
 List<Task<JobResult>> tasks = new();
 for (int i = 1; i <= 20; i++)
 {
+    // adds a job while returning a task containing the result (when complete)
     Task<JobResult> taskResult = service.AddJobAndProcessAsync(new Job(i, $"test - {i}"));
     tasks.Add(taskResult);
 }
 
-service.Shutdown(); // shutdown early but complete preadded jobs
+// jobs added after a shutdown fail while ones added before still succeed even if incomplete
+//service.Shutdown();
+//Task<JobResult> failResult = service.AddJobAndProcessAsync(new Job(0, $"test - shouldfail"));
+//tasks.Add(failResult);
 
-Task<JobResult> failResult = service.AddJobAndProcessAsync(new Job(0, $"test - shouldfail"));
-tasks.Add(failResult);
-
-Thread.Sleep(25000);
-
+// wait for all tasks to finish 
 JobResult[] results = await Task.WhenAll(tasks);
+
+//output results
 foreach (JobResult result in results)
 {
     Console.WriteLine(result.ToString());
 }
+
+// shut down the service
+service.Shutdown();
